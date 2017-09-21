ee7e86542ba6bdb06283312fcc9a9e9431f07457
quietcoder <1642965215@qq.com>
2017-09-21 15:57:11 +0800

    修改：loading的实现方式

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="td-component-loading">
-    <div class="loading-container">
-      <span v-for="i in 8" :key="i"></span>
+    <div class="loader">
+      <div class="loading-spinner">
+        <svg class="circular" viewBox="25 25 50 50">
+          <circle class="path" cx="50" cy="50" r="20" fill="none"/>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -24,81 +28,42 @@ export default {
   align-items: center;
   background-color: rgba(255, 255, 255, .8);
 
-  .loading-container {
-    width: 100px;
-    height: 100px;
-    position: relative;
+  .loading-spinner {
+    .circular {
+      width: 42px;
+      height: 42px;
+      animation: loading-rotate 2s linear infinite;
 
-    span {
-      display: inline-block;
-      width: 16px;
-      height: 16px;
-      border-radius: 50%;
-      background: #58b7ff;
-      position: absolute;
-      animation: td-component-loading-load 1.04s ease infinite;
-
-      &:nth-child(1) {
-        left: 0;
-        top: 50%;
-        margin-top:-8px;
-        animation-delay:0.13s;
-      }
-
-      &:nth-child(2) {
-        left: 14px;
-        top: 14px;
-        animation-delay:0.26s;
-      }
-
-      &:nth-child(3) {
-        left: 50%;
-        top: 0;
-        margin-left: -8px;
-        animation-delay:0.39s;
-      }
-
-      &:nth-child(4) {
-        top: 14px;
-        right:14px;
-        animation-delay:0.52s;
-      }
-
-      &:nth-child(5) {
-        right: 0;
-        top: 50%;
-        margin-top:-8px;
-        animation-delay:0.65s;
-      }
-
-      &:nth-child(6) {
-        right: 14px;
-        bottom:14px;
-        animation-delay:0.78s;
-      }
-
-      &:nth-child(7) {
-        bottom: 0;
-        left: 50%;
-        margin-left: -8px;
-        animation-delay:0.91s;
-      }
-
-      &:nth-child(8) {
-        bottom: 14px;
-        left: 14px;
-        animation-delay:1.04s;
+      .path {
+        animation: loading-dash 1.5s ease-in-out infinite;
+        stroke-dasharray: 90,150;
+        stroke-dashoffset: 0;
+        stroke-width: 2;
+        stroke: #20a0ff;
+        stroke-linecap: round;
       }
     }
   }
+}
 
-  @keyframes td-component-loading-load {
-    0% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0.2;
-    }
+@keyframes loading-rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loading-dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -40px;
+  }
+  100% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -120px;
   }
 }
 </style>
